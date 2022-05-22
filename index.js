0e2f352558d2c8c0675167b37066a712e6a003c0
devnoman01 <devnoman01@gmail.com>
2022-05-23 01:46:29 +0600

    database created, connected and sample data loaded successfully

--- index.js
+++ index.js
@@ -2,13 +2,34 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
-
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rsh38.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  await client.connect();
+  console.log("DB Connected");
+
+  const partsCollection = client.db("manufacturer_portal").collection("parts");
+
+  app.get("/parts", async (req, res) => {
+    const parts = await partsCollection.find().toArray();
+    res.send(parts);
+  });
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Manufacturer Portal Server Running");
